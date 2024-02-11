6ee6d563204dbb55901904266ee3e56f5626430b
Pall Pandiyan.S <pallpandiyan@gmail.com>
2024-02-11 17:14:50 +0530

    navbar ok

--- scripts.js
+++ scripts.js
@@ -1,13 +1,37 @@
-"use strict";
+// "use strict";
 
-const body_wrapper = document.getElementById("body-wrapper");
+// function update_style(element, new_style) {
 
-function updateOverflowSize(element) {
-    console.log("updateOverflowSize() called");
-    element.style.width = `${element.scrollWidth}px`;
-    element.style.height = `${element.scrollHeight}px`;
-}
+//     if (!element.hasOwnProperty("style")) {
+//         element.style = {};
+//     }
 
-// window.addEventListener("resize", ()=> {updateOverflowSize(body_wrapper)});
-// updateOverflowSize(body_wrapper);
-// updateOverflowSize(body_wrapper);
+//     Object.keys(new_style).forEach(key => {
+//         element.style[key] = new_style[key];
+//     });
+
+// }
+
+// const html_element = document.getElementsByName("html");
+// const body_wrapper_div = document.getElementById("body-wrapper");
+// const main_div = document.getElementById("main");
+
+
+// update_style(html_element, {
+//     "margin": "0",
+//     "padding": "0",
+// });
+
+// update_style(body_wrapper_div, {
+//     "background-color": "lightblue",
+//     "height": "100vh",
+//     "z-index": "1"
+// });
+
+
+// body_wrapper_div.style.width = `${window.width}px`;
+// body_wrapper_div.style.height = `${window.height}px`;
+// body_wrapper_div.style.border = "2px red solid";
+
+
+// main_div.style.border = "2px blue solid";
