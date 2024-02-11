e317f3e0fb808059983ba272bb2ebc6eb83da444
Pall Pandiyan.S <pallpandiyan@gmail.com>
2024-02-11 18:03:23 +0530

    cleaned the script a little

--- scripts.js
+++ scripts.js
@@ -1,17 +1,18 @@
 "use strict";
 const mobile_navbar_icon = document.getElementById("mobile_navbar_icon");
 const navbar = document.getElementById("navbar");
-const toggle_mobile_menu = () => {
+
+mobile_navbar_icon.addEventListener("click", () => {
     if (navbar.style.display === "block") {
         navbar.style.display = "none";
     }
     else {
         navbar.style.display = "block";
     }
-};
-mobile_navbar_icon.addEventListener("click", toggle_mobile_menu);
+});
 
-const auto_hide_mobile_menu = () => {
+
+navbar.addEventListener("mouseleave", () => {
     if (mobile_navbar_icon.style.display === "none") {
         return;
     }
@@ -19,5 +20,4 @@ const auto_hide_mobile_menu = () => {
         return;
     }
     navbar.style.display = "none";
-};
-navbar.addEventListener("mouseleave", auto_hide_mobile_menu);
+});
